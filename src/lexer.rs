c023377f0e3910eb4c41b1a7d737066dad67409e
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-27 01:15:28 +0900

    feat: implement number parser system

---
+++ src/lexer.rs
@@ -0,0 +1,48 @@
+use regex::Regex;
+
+pub fn parse(text: &str) {
+    let mut p = text.chars().peekable();
+
+    loop {
+        if let Some(token) = p.next() {
+            match token {
+                '+' => {
+                    println!("add: {}", token);
+                }
+                '-' => {
+                    println!("sub: {}", token);
+                }
+                '*' => {
+                    println!("mul:{}", token);
+                }
+                '/' => {
+                    println!("div:{}", token);
+                }
+                _ => match is_number(&token.to_string()) {
+                    true => {
+                        println!("number: {}", token);
+                    }
+                    false => {
+                        todo!();
+                    }
+                },
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+fn is_number(string: &str) -> bool {
+    let re_num = Regex::new(r"^\d+$").unwrap();
+    re_num.is_match(string)
+}
+
+#[test]
+fn test_number() {
+    assert_eq!(true, is_number("1234"));
+    assert_eq!(true, is_number("2934"));
+
+    assert_eq!(false, is_number("new1"));
+    assert_eq!(false, is_number("12beef334"));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use fortran_rust_compiler::lexer::parse;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Incorrect number of arguments.");
+    }
+
+    parse(args[1].to_string().as_str())
 }
